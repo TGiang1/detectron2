7bcd9900d6f73a4c25b0a4779e944ad2062b7a7d
Tommy Giang <tommy.giang@sjsu.edu>
2022-06-24 14:02:15 -0700

    Update catalog.py
    Remove assertion check for changing metadata

--- detectron2/data/catalog.py
+++ detectron2/data/catalog.py
@@ -143,14 +143,14 @@ class Metadata(types.SimpleNamespace):
             setattr(self, self._RENAMED[key], val)
 
         # Ensure that metadata of the same name stays consistent
-        try:
-            oldval = getattr(self, key)
-            assert oldval == val, (
-                "Attribute '{}' in the metadata of '{}' cannot be set "
-                "to a different value!\n{} != {}".format(key, self.name, oldval, val)
-            )
-        except AttributeError:
-            super().__setattr__(key, val)
+        # try:
+        #     oldval = getattr(self, key)
+        #     assert oldval == val, (
+        #         "Attribute '{}' in the metadata of '{}' cannot be set "
+        #         "to a different value!\n{} != {}".format(key, self.name, oldval, val)
+        #     )
+        # except AttributeError:
+        #     super().__setattr__(key, val)
 
     def as_dict(self):
         """
